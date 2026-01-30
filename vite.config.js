aee1c9bacb9c0f80aff0c361a59717dcef04fad7
Cyril Ghys <cyril.ghys@gmail.com>
2026-01-30 15:28:35 +0100

    Update project name and Vite base to limbsocketdesigner

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/instalimb/",
+	base: "/limbsocketdesigner/",
 	plugins: [react(), tailwindcss()],
 });
